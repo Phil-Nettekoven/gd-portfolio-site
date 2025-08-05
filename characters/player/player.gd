ecba32dfbb5281280e1ca46ca583e73808980ada
philn <nettekovenp@gmail.com>
2025-08-05 00:37:29 -0400

    true

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,68 +1,86 @@
 extends CharacterBody3D
 
+@onready var jump_timer:Timer = $jump_timer
+var can_jump:bool = true
 
-const SPEED = 10.0
+const SPEED:float = 10.0
+const SPRINT_MOD:float = 2.0
+
 const JUMP_VELOCITY:float = 2
+const JUMP_MOD:float = 1.0
+const JUMP_COOLDOWN:float = 0.05
 
 enum STATE {pathing, free, disabled}
-var state:STATE = STATE.pathing
+var state:STATE = STATE.free
 
 @onready var animated_sprite:AnimatedSprite3D = $AnimatedSprite3D
 var player_direction:String = "down"
 
-func path_movement(delta:float)->void:
-	# Add the gravity.
+func path_movement(_delta:float)->void:
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * _delta
 		return
+	
+	# if jump_timer.is_stopped() && not can_jump: #Start timer once the player has hit the ground again
+	# 	jump_timer.start(JUMP_COOLDOWN)
+	# 	return
 
 	velocity.x = 0
-
-	if !(Input.is_action_just_pressed("left") || Input.is_action_just_pressed("right")):return
-	var direction :Vector2= Input.get_vector("left", "right", "up", "down").normalized()
-
-	# if direction.y < 0: player_direction = "up"
-	# if direction.y > 0: player_direction = "down"
 	
-	velocity.y = JUMP_VELOCITY
+	#if !can_jump: return
+	if !(Input.is_action_pressed("left") || Input.is_action_pressed("right")): return
+	var direction:Vector2 = Input.get_vector("left", "right", "up", "down").normalized()
+	
+	var cur_movespeed:float = SPEED
+	var cur_jump_velocity:float = JUMP_VELOCITY
+	if Input.is_action_pressed("sprint"):
+		cur_movespeed *= SPRINT_MOD
+		cur_jump_velocity *= JUMP_MOD
+	
+
 	if direction:
-		velocity.x = direction.x * SPEED
+		velocity.x = direction.x * cur_movespeed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, cur_movespeed)
+	
+	if direction.x != 0:
+		velocity.y = cur_jump_velocity
+		#can_jump = false
 
-func movement(delta:float)->void:
-
-	# Add the gravity.
+func free_movement(_delta:float)->void:
+	
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * _delta
 		return
 
 	velocity.x = 0
 	velocity.z = 0
 
-	# Handle jump.
-	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	
-	# Get the input direction and handle the movement/deceleration.
 	var input_dir :Vector2= Input.get_vector("left", "right", "up", "down")
 	var direction :Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	# if direction == Vector3.ZERO:
-	# 	return
+	
+	var cur_movespeed:float = SPEED
+	var cur_jump_velocity:float = JUMP_VELOCITY
+	if Input.is_action_pressed("sprint"):
+		cur_movespeed *= SPRINT_MOD
+		cur_jump_velocity *= JUMP_MOD
 
-	velocity.y = JUMP_VELOCITY
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = direction.x * cur_movespeed
+		velocity.z = direction.z * cur_movespeed
+		velocity.y = cur_jump_velocity
+	# else:
+	# 	print("not moving?")
+	# 	velocity.x = move_toward(velocity.x, 0, SPEED)
+	# 	velocity.z = move_toward(velocity.z, 0, SPEED)
 
 func handle_animations()->void:
-	var idle_str:String = ""
+	var idle:bool = false
 	var movement_direction:String = ""
-	#var direction:String = ""
+	var animation_string:String = ""
+
 	if velocity.x == 0.0 && velocity.z == 0.0:
-		idle_str += "idle_"
+		idle = true
 
 	if velocity.x > 0:
 		movement_direction += "right"
@@ -77,14 +95,37 @@ func handle_animations()->void:
 
 	if player_direction != movement_direction: player_direction = movement_direction
 
-	animated_sprite.play(idle_str+movement_direction)
+	if idle:
+		animation_string = "idle_"
+	animation_string += movement_direction
+	animated_sprite.play(animation_string)
 
-func _physics_process(delta: float) -> void:
+func change_state(new_state_name:String)->void:
+	var new_state:STATE
+	match(new_state_name):
+		"pathing":
+			new_state = STATE.pathing
+		"free":
+			new_state = STATE.free
+		"disabled":
+			new_state = STATE.disabled
+		
+	state = new_state
+
+
+func _on_timer_timeout() -> void:
+	can_jump = true
+
+func _process(delta: float) -> void:
+
+	
 	if state == STATE.pathing:
 		path_movement(delta)
 	elif state == STATE.free:
-		movement(delta)
+		free_movement(delta)
 	else: ##DISABLED
 		return
 	handle_animations()
 	move_and_slide()
+
+
