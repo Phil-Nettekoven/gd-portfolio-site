cf3bfe5dda856dee45c909a2c9a809f679009a0e
philn <nettekovenp@gmail.com>
2025-08-08 04:03:41 -0400

    reworked movement

---
+++ characters/player/camera_pivot.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@export_range(0.0, 1.0) var mouse_sensitivity:float = 0.005
+@export var tilt_limit:float = deg_to_rad(75)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is not InputEventMouseMotion: 
+		return
+
+	rotation.x -= event.relative.y * mouse_sensitivity
+	# Prevent the camera from rotating too far up or down.
+	rotation.x = clampf(rotation.x, -tilt_limit, tilt_limit)
+	rotation.y += -event.relative.x * mouse_sensitivity
--- characters/player/player.gd
+++ characters/player/player.gd
@@ -2,16 +2,19 @@ extends CharacterBody3D
 
 @onready var jump_timer:Timer = $jump_timer
 @onready var spring_arm:SpringArm3D = $SpringArm3D
-@onready var camera:Camera3D = $SpringArm3D/Camera3D
+@onready var camera:Camera3D = %camera
 var can_jump:bool = true
 
-const SPEED:float = 10.0
+const MAX_SPEED:float  = 100.0
 const SPRINT_MOD:float = 2.0
+const ACCELERATION:float = 50
+const DECELERATION:float = 75
 
 const JUMP_VELOCITY:float = 2
 const JUMP_MOD:float = 1.25
 const JUMP_COOLDOWN:float = 0.05
 const GRAVITY_MOD:float = 2.0
+
 
 enum STATE {pathing, free, disabled}
 var state:STATE = STATE.free
@@ -23,61 +26,82 @@ func path_movement(_delta:float)->void:
 	if not is_on_floor():
 		velocity += (get_gravity() * GRAVITY_MOD) * _delta
 		return
-	
+
 	# if jump_timer.is_stopped() && not can_jump: #Start timer once the player has hit the ground again
 	# 	jump_timer.start(JUMP_COOLDOWN)
 	# 	return
 
 	velocity.x = 0
-	
+
 	#if !can_jump: return
 	if !(Input.is_action_pressed("left") || Input.is_action_pressed("right")): return
 	var direction:Vector2 = Input.get_vector("left", "right", "up", "down").normalized()
-	
-	var cur_movespeed:float = SPEED
+
+	var cur_movespeed:float =     MAX_SPEED
 	var cur_jump_velocity:float = JUMP_VELOCITY
 	if Input.is_action_pressed("sprint"):
 		cur_movespeed *= SPRINT_MOD
 		cur_jump_velocity *= JUMP_MOD
-	
-	
 
 	if direction:
 		velocity.x = direction.x * cur_movespeed
 	else:
 		velocity.x = move_toward(velocity.x, 0, cur_movespeed)
-	
+
 	if direction.x != 0:
 		velocity.y = cur_jump_velocity
 		#can_jump = false
 
 func free_movement(_delta:float)->void:
-	
 	if not is_on_floor():
 		velocity += (get_gravity() * GRAVITY_MOD) * _delta
+		move_and_slide()
 		return
-
-	velocity.x = 0
-	velocity.z = 0
-
-	#var dir = Vector3(input.x, 0, input.y).rotated(Vector3.UP, spring_arm.rotation.y)	
-	var input_dir :Vector2= Input.get_vector("left", "right", "up", "down")
-	var direction :Vector3 = Vector3(input_dir.x, 0, input_dir.y).normalized()
-	direction = direction.rotated(Vector3.UP, camera.global_position.y)
+		
+	var input_dir:Vector2 = Input.get_vector("left","right","up","down")
+	var direction:Vector3 = Vector3(input_dir.x,0,input_dir.y).normalized()
+	direction = direction.rotated(Vector3.UP, camera.global_rotation.y)
 	
-	var cur_movespeed:float = SPEED
-	var cur_jump_velocity:float = JUMP_VELOCITY
-	if Input.is_action_pressed("sprint"):
-		cur_movespeed *= SPRINT_MOD
-		cur_jump_velocity *= JUMP_MOD
-
 	if direction:
-		velocity.x = move_toward(velocity.x, direction.x, _delta*SPEED)
-		velocity.z = move_toward(velocity.z, direction.z, _delta*SPEED)
-		velocity.y = cur_jump_velocity
-	else: 
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		direction *= MAX_SPEED
+		velocity.x = move_toward(velocity.x, direction.x, _delta * ACCELERATION)
+		velocity.z = move_toward(velocity.z, direction.z, _delta * ACCELERATION)
+	else:
+		velocity.x = move_toward(velocity.x, 0, _delta * DECELERATION)
+		velocity.z = move_toward(velocity.z, 0, _delta * DECELERATION)
+
+	move_and_slide()
+#	if not (is_on_floor() and velocity.y <= 0): return
+#	
+#	if velocity.
+
+#func free_movement_old(_delta:float)->void:
+#	
+#	if not is_on_floor():
+#		velocity += (get_gravity() * 1) * _delta
+#		return
+#
+##	velocity.x = 0
+##	velocity.z = 0
+#
+#	#var dir = Vector3(input.x, 0, input.y).rotated(Vector3.UP, spring_arm.rotation.y)	
+#	var input_dir :Vector2= Input.get_vector("left", "right", "up", "down")
+#	var direction :Vector3 = Vector3(input_dir.x, 0, input_dir.y).normalized()
+#	direction = direction.rotated(Vector3.UP, camera.global_position.y)
+#	
+#	var cur_movespeed:float = SPEED
+#	var cur_jump_velocity:float = JUMP_VELOCITY
+#	if Input.is_action_pressed("sprint"):
+#		cur_movespeed *= SPRINT_MOD
+#		cur_jump_velocity *= JUMP_MOD
+#
+#	if direction:
+#		velocity.x = move_toward(velocity.x, direction.x, _delta*cur_movespeed)
+#		velocity.z = move_toward(velocity.z, direction.z, _delta*cur_movespeed)
+#		velocity.y = cur_jump_velocity
+#	else: 
+#		velocity.x = move_toward(velocity.x, 0, cur_movespeed)
+#		velocity.z = move_toward(velocity.z, 0, cur_movespeed)
 
 func handle_animations()->void:
 	var idle:bool = false
@@ -122,7 +146,6 @@ func _on_timer_timeout() -> void:
 	can_jump = true
 
 func _process(delta: float) -> void:
-
 	
 	if state == STATE.pathing:
 		path_movement(delta)
@@ -131,6 +154,5 @@ func _process(delta: float) -> void:
 	else: ##DISABLED
 		return
 	handle_animations()
-	move_and_slide()
-
-
+	
+	
