362b31e3e92d6d7b675f0646f2bfadff0be764c8
philn <nettekovenp@gmail.com>
2025-09-18 23:13:41 -0400

    everybody

--- objects/chest_1/chest_1.gd
+++ objects/chest_1/chest_1.gd
@@ -2,11 +2,14 @@ extends RigidBody3D
 
 @export var item_name:String = ""
 
+func _integrate_forces(_state: PhysicsDirectBodyState3D) -> void:
+	return
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
