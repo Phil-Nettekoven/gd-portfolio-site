7fe02016e5133859cdb29d5ac5e9b76150ef74dc
philn <nettekovenp@gmail.com>
2025-09-26 11:45:26 -0400

    freelance police

--- landing_page/objects/button_1/button_1.gd
+++ landing_page/objects/button_1/button_1.gd
@@ -7,9 +7,13 @@ var cur_state:STATE = STATE.INACTIVE
 
 func look_at_mouse()->void:
 	var mouse_position:Vector2 = get_viewport().get_mouse_position()
-	var target_position:Vector3 = Vector3(mouse_position.x, self.global_position.y, 0)
+	#var target_position:Vector3 = Vector3(mouse_position.x, mouse_position.y, 10)
 
-	self.look_at(target_position)
+	
+	#var mouse_position:Vector2 = DisplayServer.mouse_get_position()
+	var target_position:Vector3 = Vector3(10, mouse_position.x, mouse_position.y)
+
+	self.look_at(target_position, Vector3.UP, true)
 
 func _ready() -> void:
 	pass # Replace with function body.
---
+++ landing_page/scenes/landng_page.gd
@@ -0,0 +1,7 @@
+extends Node3D
+
+#@onready var buttons_folder:Node3D = $buttons
+
+func _ready() -> void:
+    pass
+    #buttons_folder.rotation_degrees = Vector3(0,90,0)
