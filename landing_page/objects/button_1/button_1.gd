6836c96b0a8aec993e91c2c2c38198f6b0e685bb
philn <nettekovenp@gmail.com>
2025-10-06 02:57:24 -0400

    button 2

--- landing_page/objects/button_1/button_1.gd
+++ landing_page/objects/button_1/button_1.gd
@@ -12,7 +12,9 @@ var acceleration: float = 200
 
 const MOUSE_Z: float = 10
 
-enum STATE {INACTIVE, ACTIVE, PRESSED}
+var mouse_in_button:bool = false
+
+enum STATE {INACTIVE, HOVERED, PRESSED}
 var cur_state: STATE = STATE.INACTIVE
 
 func _ready() -> void:
@@ -29,5 +31,29 @@ func rotation_velocity(_delta: float) -> void:
 	var target_rotation: Vector3 = rotate_controller.rotation_degrees
 	animated_sprite.rotation_degrees = target_rotation
 
+func _on_area_3d_mouse_entered() -> void:
+	cur_state = STATE.HOVERED
+	#animated_sprite.play("hovered")
+	mouse_in_button = true
+
+func _on_area_3d_mouse_exited() -> void:
+	cur_state = STATE.INACTIVE
+	animated_sprite.play("inactive")
+	mouse_in_button = false
+
+func _input(_event: InputEvent) -> void:
+	if !(_event is InputEventMouseButton):
+		return
+
+	if Input.is_action_just_pressed("left_click"):
+		if cur_state == STATE.HOVERED:
+			cur_state = STATE.PRESSED
+			animated_sprite.play("pressed")
+	elif Input.is_action_just_released("left_click"):
+		if cur_state == STATE.PRESSED:
+			cur_state = STATE.HOVERED
+			animated_sprite.play("inactive")
+	
+	
 func _physics_process(_delta: float) -> void:
 	rotation_velocity(_delta)
