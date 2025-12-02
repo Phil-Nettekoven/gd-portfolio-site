09628529b3a66e7435df402a05b84ad223b53b90
philn <nettekovenp@gmail.com>
2025-12-02 05:42:17 -0500

    tempo for sega 32x

--- act_2/scenes/world_1/bgm_mgr.gd
+++ act_2/scenes/world_1/bgm_mgr.gd
@@ -4,84 +4,20 @@ extends Node
 @onready var player_char:CharacterBody3D = %player
 var player_touched_ground:bool = false
 
-@onready var drums:AudioStreamPlayer = $drums
-@onready var bass:AudioStreamPlayer = $bass
-@onready var chords:AudioStreamPlayer = $chords
-@onready var leads:AudioStreamPlayer = $leads
-
-@export var drum_audio:AudioStream
-@export var bass_ogg:AudioStream
-@export var chords_ogg:AudioStream
-@export var leads_ogg:AudioStream
-
-@onready var audio_streams:Array[AudioStreamPlayer] = [drums,bass,chords,leads]
-@onready var animation_player:AnimationPlayer = $AnimationPlayer
-const ANIMATION_NAME:String = "lerp_music_pitch"
-const MAX_PITCH_SCALE:float = 1.0
-
-var is_playing:bool = true
-var prev_speed:float = 0.0
+@export var bgm_stream:AudioStream
+@onready var bgm_player:AudioStreamPlayer = $bgm_player
 
 func _ready() -> void:
-	if !drum_audio:
-		Globals.gprint("No audio file for BGM provided.")
+	if !bgm_stream:
+		Globals.gprint("No BGM provided in scene world_1")
 		return
-	drum_audio.loop = true
-	drums.stream = drum_audio
-	#drums.finished.connect(_on_drums_finished)
-	
-	#bass_mp3.loop = true
-	#bass.stream = bass_mp3
-	
-	#chords_mp3.loop = true
-	#chords.stream = chords_mp3
-	
-	#leads_mp3.loop = true
-	#leads.stream = leads_mp3
-
-	#animation_player.play(ANIMATION_NAME)
-	for p:AudioStreamPlayer in audio_streams:
-		p.play()
+	bgm_player.stream = bgm_stream
+	bgm_player.finished.connect(_on_bgm_finished)
 	
 	player_char.just_touched_ground.connect(_on_ground_touched)
 	
 func _on_ground_touched()->void:
-	player_touched_ground = true
+	bgm_player.play()
 
-func pause_all()->void:
-	if !is_playing: return
-	drums.stream_paused = true
-
-	is_playing = false
-	
-func resume_all()->void:
-	if is_playing: return
-	drums.stream_paused = false
-#	for p:AudioStreamPlayer in audio_streams:
-#		p.stream_paused = false
-	is_playing = true
-	
-func modulate_pitch_by_velocity()->void:
-	if !player_touched_ground: return
-	
-	var speed:float = player_char.velocity.length()
-	if speed <= 0.0:
-		pause_all()
-		return
-		
-	if !is_playing:
-		resume_all()
-		
-	var speed_ratio:float = clamp(speed/Globals.MUSIC_MAX_SPEED, 0.0, 0.99)
-	prev_speed = speed
-	animation_player.seek(speed_ratio)
-
-#func _on_drums_finished()->void:
-#	drums.seek(0.0)
-#	drums.play()
-##	for p:AudioStreamPlayer in audio_streams:
-##		p.seek(0.0)
-##		p.play()
-
-func _process(_delta: float) -> void:
-	modulate_pitch_by_velocity()
+func _on_bgm_finished()->void:
+	bgm_player.play()
