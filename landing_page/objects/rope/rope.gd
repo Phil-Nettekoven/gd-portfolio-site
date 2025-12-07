6221179925052461bf3d344de16379c7c50ddfe3
philn <nettekovenp@gmail.com>
2025-12-07 08:33:44 -0500

    more epic rope action
    that i'll undo later

--- landing_page/objects/rope/rope.gd
+++ landing_page/objects/rope/rope.gd
@@ -1,23 +1,50 @@
-extends PinJoint3D
+extends Path3D
 
-@export var rope_path: Path3D
 @export_range(1, 100) var num_links: int = 1
 @export var color: Color = Color.RED
-
+@export var hanger_scene: PackedScene
+var rope_link_scene: PackedScene = load('res://landing_page/objects/rope/rope_link.tscn')
 
 var rope_length: float = 0.0
 var link_size: float = 0.0
-
+var joints:Array[PinJoint3D]
+var links:Array[RigidBody3D]
+var hanger:RigidBody3D
 
 func _ready() -> void:
-	assert(rope_path, "No rope path detected for " + self.name)
-	determine_link_length()
-
+	assert(self.curve, "No curve detected for rope " + self.name)
+	assert(hanger_scene, "No object attached as hanger_scene in " + self.name)
+	generate_rope()
 	
-func determine_link_length() -> void:
-	rope_length = rope_path.curve.get_baked_length()
-	link_size = rope_length / num_links
+func generate_rope()->void:
+	var points:PackedVector3Array = self.curve.get_baked_points()
+	var i:int = 0
+	
+	for p:Vector3 in points:
+		var new_joint:PinJoint3D = PinJoint3D.new()
+		var new_link:RigidBody3D = rope_link_scene.instantiate()
 
+		joints.append(new_joint)
+		links.append(new_link)
+		
+		
+
+		self.add_child(new_joint)
+		self.add_child(new_link)
+		
+		var collision_shape:CollisionShape3D = new_link.collision_shape
+		collision_shape.shape.height = 1.0
+		new_joint.position = p
+
+		if i > 0:
+			new_joint.node_a = links[i-1].get_path()
+		
+		new_joint.node_b = new_link.get_path()
+
+		i += 1
+	
+	hanger = hanger_scene.instantiate()
+	joints[len(joints)-1].node_b = hanger.get_path()
 
 func _process(_delta: float) -> void:
 	pass
---
+++ landing_page/objects/rope/rope_link.gd
@@ -0,0 +1,3 @@
+extends RigidBody3D
+
+@onready var collision_shape:CollisionObject3D = $CollisionShape3D
